c1bf81f1355d69f8fd63ee2d331666b61aa3558f
Rfserveis <david@rfserveis.com>
2025-12-15 07:36:34 +0100

    Delete src directory

--- src/services/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://lwtsdtjiwfvurquddfqd.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx3dHNkdGppd2Z2dXJxdWRkZnFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ1ODMwNTIsImV4cCI6MjA4MDE1OTA1Mn0.aE9OqNLORZs1HhQsjfqNymabkNQJizAkwVanx0D19NU'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
